8afaedca5c938c3bebe3e54eb32e6e08759815a8
AdleJS <askaradilet41@gmail.com>
2025-02-08 16:54:13 +0500

    changed configuration file to match chrome browser, added test descriptions to test report

--- tests/recorded.spec.ts
+++ tests/recorded.spec.ts
@@ -1,12 +1,23 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://github.com/');
-  await page.getByRole('link', { name: 'Sign in' }).click();
-  await page.getByRole('textbox', { name: 'Username or email address' }).click();
-  await page.getByRole('textbox', { name: 'Username or email address' }).fill('testers@mail.com');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('test123');
-  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
-  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+  await test.step('Navigating to URL', async () => {
+    await page.goto('https://github.com/');
+    await page.getByRole('link', { name: 'Sign in' }).click();
+  });
+
+  await test.step('Enter username and password', async () => {
+    await page.getByRole('textbox', { name: 'Username or email address' }).click();
+    await page.getByRole('textbox', { name: 'Username or email address' }).fill('testers@mail.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('test123');
+  });
+
+  await test.step('Click on Sign in button', async () => {
+    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  });
+
+  await test.step('Validate error message', async () => {
+    await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+  });
 });
