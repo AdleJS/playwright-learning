bd9f2c916f06f79973ee60b72b6617747de72238
AdleJS <askaradilet41@gmail.com>
2025-02-10 16:46:23 +0500

    researched different locator types

--- tests/chapter2/locatorsTest.spec.ts
+++ tests/chapter2/locatorsTest.spec.ts
@@ -2,8 +2,12 @@ import { test, expect } from "@playwright/test";
 
 test("Locators Test", async ({ page }) => {
     await page.goto("https://github.com/BakkappaN");
-    // await page.getByRole("link", { name: "Sign in" });
-    // await page.getByLabel("Homepage", { exact: true }).first().click();
-    // await page.getByAltText("View BakkappaN's full-sized avatar", { exact: true }).click();
+    await page.getByRole("link", { name: "Sign in" });
+    await page.getByLabel("Homepage", { exact: true }).first().click();
+    await page.getByAltText("View BakkappaN's full-sized avatar", { exact: true }).click();
     await page.getByTestId("repositories").first().click();
+    await page.getByText("Packages").first().click();
+    await page.getByPlaceholder("Search").fill("New repository");
+    await page.locator("//input[@name='commit']").first().click();
+    await page.locator("input[name='commit']").first().click();
 });
