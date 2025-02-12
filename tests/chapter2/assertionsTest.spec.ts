b369d5c87ad69722c7f5285a5110d568c316e0a8
AdleJS <askaradilet41@gmail.com>
2025-02-12 17:56:37 +0500

    added watch mode and soft assertions test

--- tests/chapter2/assertionsTest.spec.ts
+++ tests/chapter2/assertionsTest.spec.ts
@@ -5,10 +5,13 @@ test("Assertions", async ({ page }) => {
     await expect(page).toHaveURL("https://tengrinews.kz/");
     await expect(page).toHaveTitle("Главные новости Казахстана - Tengrinews.kz");
 
-    // await page.getByRole('img', { name: 'Поиск' }).click();
-    // await expect(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toBeVisible();
-    // await expect(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toBeEditable();
-    // await expect(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toBeEnabled();
-    // await expect(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toBeEmpty();
-    // await expect(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toBeDisabled();
+    await page.getByRole('img', { name: 'Поиск' }).click();
+    await expect.soft(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toBeVisible();
+    await expect.soft(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toBeEditable();
+    await expect.soft(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toBeEnabled();
+    await expect.soft(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toBeEmpty();
+    await expect.soft(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toBeDisabled();
+    await expect.soft(page.getByRole('searchbox', { name: 'Найти в TengriNews' })).toHaveCount(1);
+
+
 });
